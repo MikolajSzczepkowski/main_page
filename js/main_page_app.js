32bab2e43a786f3b505023f526a8743d44ec9448
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-11-17 17:47:53 +0100

    js folder

---
+++ js/main_page_app.js
@@ -0,0 +1,48 @@
+$(function (){
+	var percent = 0,
+		bar = $('.transition-timer-carousel-progress-bar'),
+		crsl = $('#myCarousel');
+
+	$( "#calendar" ).datepicker({showOtherMonths: true,
+					selectOtherMonths: true,
+					firstDay: 1
+	});
+	
+	function progressBarCarousel() {
+		bar.css({width:percent+'%'});
+		percent = percent +0.5;
+			if (percent>100) {
+				percent=0;
+				crsl.carousel('next');
+				}      
+			}
+	crsl.carousel({
+		interval: false,
+		pause: true
+	}).on('slid.bs.carousel', function () {percent=0;});var barInterval = setInterval(progressBarCarousel, 30);
+	crsl.hover(
+		function(){
+			clearInterval(barInterval);
+			},
+		function(){
+			barInterval = setInterval(progressBarCarousel, 30);
+	});
+	
+	$(".newest").on("mouseenter", function(){
+		$(this).append("<div class='newest-hover'></div>");
+		$(".newest-hover").append("<p class='text-center text-uppercase'>visit</p>");
+		$(".newest-hover").append("<p class='text-center text-uppercase'>profile</p>");	
+	});
+	$(".newest").on("mouseleave", function(){
+		$(".newest-hover").remove();
+	});
+	$(".newest-tournament").on("mouseenter", function(){
+		$(this).append("<div class='newest-hover text-center'></div>");
+		$(".newest-hover").append("<img src='images/cup.png' alt='cup'/>");
+		$(".newest-hover").append("<p class='text-uppercase'>enter</p>");
+		$(".newest-hover").append("<p class='text-uppercase'>tournament</p>");	
+	});
+	$(".newest-tournament").on("mouseleave", function(){
+		$(".newest-hover").remove();
+	});
+});
