64937f8e75c5db66e5420ca0a220274990b4929a
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-12-02 15:47:13 +0100

    menu dropdown playstation xbox

--- js/main_app.js
+++ js/main_app.js
@@ -1,21 +1,27 @@
 $(function (){
 	var pageHeight = $(window).height()-195;
-		pageWidth = $(document).width();
-
+		pageWidth = $(document).width(),
+		menuBoxHeight = $("#firstMenuBox").height()+20;
+console.log(menuBoxHeight);
 	if(pageWidth>=990){
 		$(".friends-list").css("height", pageHeight);
+		$("#secondMenuBox").css("margin-top", "5px");
 	}
 	else{
 		$(".friends-list").css("height", "400px");
+		$("#secondMenuBox").css("margin-top", menuBoxHeight);
 	}
 	$(window).resize(function(){
 		pageHeight = $(window).height()-195,
     	pageWidth = $(document).width();
+    	menuBoxHeight = $("#firstMenuBox").height()+20;
     	if(pageWidth>=990){
 			$(".friends-list").css("height", pageHeight);
+			$("#secondMenuBox").css("margin-top", "5px");
 		}
 		else{
 			$(".friends-list").css("height", "400px");
+			$("#secondMenuBox").css("margin-top", menuBoxHeight);
 		}
 	});
 
