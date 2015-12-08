a8915147693d4dd21406d1212645a8d4e6b99de9
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-12-08 15:20:28 +0100

    left chat display

--- js/main_app.js
+++ js/main_app.js
@@ -1,7 +1,9 @@
 $(function (){
 	var pageHeight = $(window).height()-186;
 		pageWidth = $(document).width(),
-		menuBoxHeight = $(document).find("#firstMenuBox").height()+20;
+		menuBoxHeight = $(document).find("#firstMenuBox").height()+20,
+		leftChatPosition = null,
+		chatBoxCounter =0;
 
 	if(pageWidth>=990){
 		$(".friends-list").css("height", pageHeight);
@@ -14,7 +16,7 @@ $(function (){
 	}
 	$(window).resize(function(){
 		pageHeight = $(window).height()-195,
-		pageWidth = $(document).width();
+		pageWidth = $(document).width(),
 		menuBoxHeight = $("#firstMenuBox").height()+20;
 		if(pageWidth>=990){
 			$(".friends-list").css("height", pageHeight);
@@ -29,9 +31,15 @@ $(function (){
 		var $this = $(this);
 		if (!$this.hasClass("panel-collapsed")) {
 			$this.parents(".panel").find(".chat-body").slideUp();
-			$this.parents(".chat-window").animate({top: "285px"});
 			$this.addClass("panel-collapsed");
 			$this.removeClass("glyphicon-minus").addClass("glyphicon-plus");
+			if ($this.parents(".chat-window").hasClass("left-chat-window")) {
+				$this.parents(".chat-window").animate({top: "273px"});
+				console.log("heja")
+			}
+			else{
+				$this.parents(".chat-window").animate({top: "285px"});
+			}
 		} else {
 			$this.parents(".panel").find(".chat-body").slideDown();
 			$this.parents(".chat-window").animate({top: "0"});
@@ -42,12 +50,13 @@ $(function (){
 
 	$(document).on("click", ".icon-close", function() {
 		var $this = $(this);
-		var $thisId = $this.parents(".chat-window").attr("id");
-		$("#"+ $thisId).remove();
+		var $thisId = $this.parents("div.chat-window").attr("id");
+		$("div#"+ $thisId).remove();
+		$("a#"+ $thisId).removeClass("clicked");
 	});
 	$(document).on("click", ".friends-list li", function() {
 		var ul = "<ul class='row friends-list-buttons'></ul>",
-			messageButton = "<li id='chatOn'><a href='chat.html'><img src='images/message.png' alt='massage' class='friends-list-buttons'></a></li>",
+			messageButton = "<li id='chatOn'><a id='player1' href='chat.html'><img src='images/message.png' alt='massage' class='friends-list-buttons'></a></li>",
 			userButton = "<li><img src='images/user.png' alt='user' class='friends-list-buttons'></li>",
 			addUserButton = "<li><img src='images/add-user.png' alt='add user' class='friends-list-buttons'></li>";
 		if (!$(this).parent().hasClass("friends-list-buttons")) {
@@ -59,16 +68,33 @@ $(function (){
 
 	$(document).on("click", "#chatOn a", function(e){
 		e.preventDefault();
-		var url = this.href;
-
-		$.ajax({
-			url: url,
-			dataType: "html",
-			type: "GET",
-			success: function(data){
-				$("#chatContainer").append($(data).find(".chat-window"));
-			}
-		});
+		var url = this.href,
+			id = $(this).attr("id");
+		if (!$(this).hasClass("clicked")) {
+			$.ajax({
+				url: url,
+				dataType: "html",
+				type: "GET",
+				success: function(data){		
+					leftChatPosition = $(".chat-window").eq(0).offset();		
+					console.log(leftChatPosition);
+					if (chatBoxCounter > 3) {
+						$("#chatContainer").append($(data).find(".chat-window"));
+						$(".chat-window").last().addClass("left-chat-window");
+					}
+					else{
+						$("#chatContainer").append($(data).find(".chat-window"));
+					}
+				}
+			});
+			$(this).addClass("clicked");
+			chatBoxCounter ++;
+		} 
+		else{
+			$("#chatContainer").find("div#"+id).remove();
+			$(this).removeClass("clicked");
+		}
+		console.log(chatBoxCounter);
 	});
 
 	$("#chat").on("click", function(){
