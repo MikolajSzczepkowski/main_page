fec2bfbfc4c5967527ef96d9c977314119e130f5
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-12-29 21:52:12 +0100

    menu background fix

--- js/main_app.js
+++ js/main_app.js
@@ -1,7 +1,6 @@
 $(function (){
 	var pageHeight = $(window).height()-186,
 		pageWidth = $(document).width(),
-		menuBoxHeight = $(document).find("#firstMenuBox").height()+20,
 		chatBoxCounter = 0,
 		allFriends = [
 			{id: "Depsperados_PL",
@@ -30,24 +29,18 @@ $(function (){
 
 	if(pageWidth>=990){
 		$(".friends-list").css("height", pageHeight);
-		$("#secondMenuBox").css("margin-top", "5px");
-		$("#menuBackground").css("height", (menuBoxHeight-16));
 	}
 	else{
 		$(".friends-list").css("height", "400px");
-		$("#secondMenuBox").css("margin-top", menuBoxHeight);
 	}
 	$(window).resize(function(){
 		pageHeight = $(window).height()-195,
 		pageWidth = $(document).width(),
-		menuBoxHeight = $("#firstMenuBox").height()+20;
 		if(pageWidth>=990){
 			$(".friends-list").css("height", pageHeight);
-			$("#secondMenuBox").css("margin-top", "5px");
 		}
 		else{
 			$(".friends-list").css("height", "400px");
-			$("#secondMenuBox").css("margin-top", menuBoxHeight);
 		}
 	});
 
